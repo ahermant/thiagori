3c43ab2a926326722f5b4724d8a098152de531a1
Aymeric <aymerichermant@gmail.com>
2020-01-26 16:09:11 -0500

    Fix form alert bug

--- components/RSVP.vue
+++ components/RSVP.vue
@@ -120,7 +120,7 @@ export default {
   methods: {
     onSubmit(evt) {
       evt.preventDefault();
-      alert(this.$("thank_you_for_submission"));
+      alert(this.$t("thank_you_for_submission"));
     },
     onReset(evt) {
       evt.preventDefault();
