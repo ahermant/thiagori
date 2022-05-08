d60a54b69cac8d7ca3f6f3d23734cb92a15b5a03
Aymeric <aymerichermant@gmail.com>
2022-05-08 15:09:31 -0400

    Dates, calendar and videos update

--- components/WeddingCountdown.vue
+++ components/WeddingCountdown.vue
@@ -4,29 +4,45 @@
     <b-card-title title-tag="h2" class="p-4 mt-0 mb-5 backgroundColor">
       <countdown :time="milliseconds">
         <template slot-scope="props">
-          <p class="mb-4">{{$t('time_remaining')}}</p>
+          <p class="mb-4">{{ $t("time_remaining") }}</p>
           <b-row>
             <b-col lg="3">
               <b-card class="thiagoriCard" body-class="text-center">
-                <b-card-title title-tag="h2" class="text-capitalize" :title="$t('days')" />
+                <b-card-title
+                  title-tag="h2"
+                  class="text-capitalize"
+                  :title="$t('days')"
+                />
                 <b-card-text class="countdown-text" v-html="props.days" />
               </b-card>
             </b-col>
             <b-col lg="3">
               <b-card class="thiagoriCard" body-class="text-center">
-                <b-card-title title-tag="h2" class="text-capitalize" :title="$t('hours')" />
+                <b-card-title
+                  title-tag="h2"
+                  class="text-capitalize"
+                  :title="$t('hours')"
+                />
                 <b-card-text class="countdown-text" v-html="props.hours" />
               </b-card>
             </b-col>
             <b-col lg="3">
               <b-card class="thiagoriCard" body-class="text-center">
-                <b-card-title title-tag="h2" class="text-capitalize" :title="$t('minutes')" />
+                <b-card-title
+                  title-tag="h2"
+                  class="text-capitalize"
+                  :title="$t('minutes')"
+                />
                 <b-card-text class="countdown-text" v-html="props.minutes" />
               </b-card>
             </b-col>
             <b-col lg="3">
               <b-card class="thiagoriCard" body-class="text-center">
-                <b-card-title title-tag="h2" class="text-capitalize" :title="$t('seconds')" />
+                <b-card-title
+                  title-tag="h2"
+                  class="text-capitalize"
+                  :title="$t('seconds')"
+                />
                 <b-card-text class="countdown-text" v-html="props.seconds" />
               </b-card>
             </b-col>
@@ -41,7 +57,7 @@
 export default {
   data() {
     return {
-      date: "28/11/2020 17:00:00"
+      date: "19/11/2022 16:00:00",
     };
   },
   computed: {
@@ -63,8 +79,8 @@ export default {
     milliseconds() {
       let timestamp = this.weddingTimestamp - Date.now();
       return timestamp;
-    }
-  }
+    },
+  },
 };
 </script>
 
